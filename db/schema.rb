de79ccd502be525ec57c90857d1f3db7031dddd8
Will Tomlins <will@tomlins.org.uk>
2014-04-23 11:58:38 +0800

    Subject has intro

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140422050536) do
+ActiveRecord::Schema.define(version: 20140423035607) do
 
   create_table "questions", force: true do |t|
     t.integer  "subject_id"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20140422050536) do
     t.datetime "updated_at"
     t.string   "slug"
     t.string   "title"
+    t.text     "intro"
   end
 
   add_index "subjects", ["slug"], name: "index_subjects_on_slug", using: :btree
--- features/step_definitions/subject_steps.rb
+++ features/step_definitions/subject_steps.rb
@@ -1,5 +1,5 @@
 Given(/^a subject titled "(.*?)"$/) do |title|
-  create(:subject, title: title)
+  create(:subject, title: title, intro: "This is the subject intro")
 end
 
 When(/^I view the subject "(.*?)"$/) do |title|
@@ -10,4 +10,5 @@ end
 Then(/^I can see the introduction information for the subject "(.*?)"$/) do |title|
   subject = Subject.find_by_title(title)
   page.should have_content subject.title
+  page.should have_content subject.intro
 end
