9861898622e6caef7c5562eea3c895e487a05d8c
Will Tomlins <will@tomlins.org.uk>
2014-04-29 13:51:02 +0800

    Removed SubjectPresenter

--- app/presenters/subject_presenter.rb
+++
@@ -1,9 +0,0 @@
-class SubjectPresenter
-  attr_accessor :subject
-
-  delegate :slug, :to_param, :title, :intro, :questions, to: :subject
-
-  def initialize(subject)
-    @subject = subject
-  end
-end
