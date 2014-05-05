3e3a4ff795f929c89c394b84ff07926defd08553
Will Tomlins <will@tomlins.org.uk>
2014-05-05 17:12:22 +0800

    Question factory doesn't make extra subjects

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :question do
     sequence(:text) { |n| "Should you answer question #{n}?" }
     sequence(:key) { |n| "answer_question_#{n}?" }
-    association :subject, factory: :subject
+    subject { build(:subject) }
   end
 
   factory :yes_no_question, parent: :question, class: YesNoQuestion do
